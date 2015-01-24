c7d5461dfcde48a0151789907a742cd53d4f193c
Milan Tair <milan.tair@gmail.com>
2015-01-24 18:35:50 +0100

    Create an initial temporary index
    I have created an initial index (only temporary).

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+	# Initial index (temp)
+
+	ob_clean();
+	header('Content-type: text/json; charset=utf-8');
+	$data = [
+		'timestamp' => date('r'),
+		'reference' => rand(1000000, 9999999),
+		'ipaddress' => $_SERVER['REMOTE_ADDR']
+	];
+	echo json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+	exit();
