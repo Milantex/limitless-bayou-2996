1d896903ca76c3b54ee9b8179c7b69d222495254
Milan Tair <milan.tair@gmail.com>
2016-05-07 09:13:56 +0200

    Finish the findOne and findMany action handler methods
    The findOne and findMany action handler methods create an SQL query
    string based on the name of the table of the currently used map and the
    where cluase generated by the parser mechanism in the BaseAction class.

--- sys/actions/FindManyAction.php
+++ sys/actions/FindManyAction.php
@@ -1,6 +1,13 @@
 <?php
     class FindManyAction extends BaseAction {   
         public function handle(stdClass $actionSpecification) {
-
+            $clause = $this->parseActionSpecification($actionSpecification);
+            $sql = 'SELECT * FROM `' . $this->getMap()->getTableName() . '` WHERE 1 AND ' . $clause . ';';
+            $result = DataBase::selectMany($sql);
+            if ($result !== NULL) {
+                new ApiResponse(ApiResponse::STATUS_OK, $result);
+            } else {
+                new ApiResponse(ApiResponse::STATUS_ERROR, 'API request execution error.');
+            }
         }
     }
--- sys/actions/FindOneAction.php
+++ sys/actions/FindOneAction.php
@@ -1,8 +1,13 @@
 <?php
-    class FindOneAction extends BaseAction {   
+    class FindOneAction extends BaseAction {
         public function handle(stdClass $actionSpecification) {
-            // TODO: Parse predefined findOne action specification
-            echo json_encode($actionSpecification);
-            exit;
+            $clause = $this->parseActionSpecification($actionSpecification);
+            $sql = 'SELECT * FROM `' . $this->getMap()->getTableName() . '` WHERE 1 AND ' . $clause . ' LIMIT 0, 1;';
+            $result = DataBase::selectOne($sql);
+            if ($result !== NULL) {
+                new ApiResponse(ApiResponse::STATUS_OK, $result);
+            } else {
+                new ApiResponse(ApiResponse::STATUS_ERROR, 'API request execution error.');
+            }
         }
     }
