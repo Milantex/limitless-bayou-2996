5af2ccdf7baa0e9f4cf860e5445c4e23661f4a19
Milan Tair <milan.tair@gmail.com>
2016-06-01 23:16:22 +0200

    Finish the add action handler
    The add action handler requires the action specification object to
    contain exactly two properties. One must be named find and the other
    must be named values. The find parameter contains an action
    specification object of its own which is parsed to give the WHERE
    clause of the SQL query and the values parameter is an object with its
    properties named after table fields of the requested map (table) and
    their values are the new values to be set (modified) for all records
    which are matched by the WHERE clause generated by parsing the action
    specification object in the find parameter.
    
    An example of the API request call for this action is:
    {
      "edit": {
        "find" : {
          "_or" : [
            { "post_id" : { "_eq" : 3 } },
            { "user_id" : { "_ne" : 1 } }
          ]
        },
        "values" : {
          "user_id": 2,
          "title": "Edited via the API",
          "link": "edited-via-the-api",
          "content": "This post was added and then edited via the API"
        }
      }
    }
    
    This request will modify values of a record with the post_id 3 which is
    not owned by the user with the user_id 1. The new values are given in
    the properties of the object specified under the 'values' property.

--- sys/actions/EditAction.php
+++ sys/actions/EditAction.php
@@ -6,6 +6,58 @@
      */
     class EditAction extends BaseAction {   
         public function handle(stdClass $actionSpecification) {
+            $this->checkActionSpecificationValidity($actionSpecification);
 
+            $actionParameters = new ActionParameters();
+            $clause = $this->parseActionSpecification($actionSpecification->find, $actionParameters);
+            $data = $actionParameters->getParameters();
+
+            $fields = [];
+
+            $uniqueIndex = 1;
+            foreach (get_object_vars($actionSpecification->values) as $key => $value) {
+                if (!$this->getMap()->fieldExists($key)) {
+                    new ApiResponse(ApiResponse::STATUS_ERROR, 'The specified field:' . addslashes($key) . ' does not exist in this map.');
+                } elseif (!$this->getMap()->getField($key)->isValid($value)) {
+                    new ApiResponse(ApiResponse::STATUS_ERROR, 'The value for the field:' . addslashes($key) . ' is not valid.');
+                } else {
+                    $fields[] = '`' . $key . '` = ' . ':update_' . $key . $uniqueIndex;
+                    $data[':update_' . $key . $uniqueIndex] = $value;
+                    $uniqueIndex++;
+                }
+            }
+
+            $sql = 'UPDATE `' . $this->getMap()->getTableName() . '` SET ' . implode(', ', $fields) . ' WHERE 1 AND ' . $clause . ';';
+            $res = DataBase::execute($sql, $data);
+
+            if ($res) {
+                $rowCount = DataBase::getLastExecutionAffectedRownCount();
+                new ApiResponse(ApiResponse::STATUS_OK, $rowCount);
+            } else {
+                new ApiResponse(ApiResponse::STATUS_ERROR, DataBase::getLastExecutionError());
+            }
+        }
+
+        /**
+         * Checks if the action specification object is of valid structure
+         * @param stdClass $actionSpecification
+         */
+        private function checkActionSpecificationValidity(stdClass $actionSpecification) {
+            $vars = get_object_vars($actionSpecification);
+            if (count($vars) != 2) {
+                new ApiResponse(ApiResponse::STATUS_ERROR, "This action's action specification object must have exactly two properties.");
+            }
+
+            if (!property_exists($actionSpecification, 'values')) {
+                new ApiResponse(ApiResponse::STATUS_ERROR, "This action's action specification object must have the 'values' property.");
+            }
+
+            if (!is_object($actionSpecification->values)) {
+                new ApiResponse(ApiResponse::STATUS_ERROR, "The 'values' property of the action specification object must have be an object.");
+            }
+
+            if (!property_exists($actionSpecification, 'find')) {
+                new ApiResponse(ApiResponse::STATUS_ERROR, "This action's action specification object must have the 'find' property.");
+            }
         }
     }
